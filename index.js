fc2429ffab15e05cb9c15e58fd9f8365c4103f66
malarosa <71916719+malarosa@users.noreply.github.com>
2020-12-15 08:02:25 +0800

    Add files via upload
    dzae wag muna lng pansinin yung index.js heheh

---
+++ index.js
@@ -0,0 +1,7 @@
+  
+console.log("hello!");
+function Submitform() {
+  var one = document.getElementById("name").value;
+  var two = document.getElementById("email").value;
+  alert("Thank you " + one + " " + two + ", We received your message.");
+}
